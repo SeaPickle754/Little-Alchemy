540434f15107776740a0950831d16a8518019ba2
SeaPickle <uraniumpjs@gmail.com>
2023-12-20 20:56:58 -0700

    Added more combonations

--- new_element.py
+++ new_element.py
@@ -1,20 +1,26 @@
-import csv
 isDebug = input("Are you formating the debug file (Blank for yes)")
+isWorking = True
 if isDebug == "" or isDebug == "y":
-    with open("bin/Debug/resources/offsets.csv" , "a") as offsetFile:
-        offsetWriter = csv.writer(offsetFile)
-        offsetFile.write("\n")
-        offset = input("Enter the offset of the new element, serparated by a comma: ")
-        offset = offset.split(',')
-        name = input("Enter the name of the new element, as a string: ")
-        offsetWriter.writerow([offset[0],offset[1],name])
-        print("Wrote offset sucessfully. 0 errors.")
-    with open("bin/Debug/resources/Combinations.csv", "a") as comboFile:
-        comboWriter = csv.writer(comboFile)
-        comboFile.write("\n")
-        offset = input("Enter the combonation of the new element, separated by a comma: ")
-        offset = offset.split(',')
-        product = input("Enter the product of those two reagents: ")
-        comboWriter.writerow([offset[0], offset[1], product])
-        print("Wrote products successfully.")
-     
+    while isWorking:
+        with open("bin/Debug/resources/offsets.csv" , "a") as offsetFile:
+            offsetFile.write("\n")
+            offset = input("Enter the offset of the new element, serparated by a comma: ")
+            offset = offset.split(',')
+            name = input("Enter the name of the new element, as a string: ")
+            string = f"{offset[0]},{offset[1]},{name}"
+            string.strip("\n")
+            offsetFile.write(string)
+            print("Wrote offset sucessfully. 0 errors.")
+        with open("bin/Debug/resources/Combinations.csv", "a") as comboFile:
+            offset = input("Enter the combonation of the new element, separated by a comma: ")
+            offset = offset.split(',')
+            product = input("Enter the product of those two reagents: ")
+            string = f"{offset[0]},{offset[1]},{product}"
+            string.strip("\n")
+            string = f"\n{string}"
+            comboFile.write(string)
+            print("Wrote products successfully.")
+        if input("Continue (Blank for yes)") == "":
+            isWorking = True
+        else:
+            isWorking = False
